c1a947c815fe9fc7929b6a36494ba7adb7ceb596
Shu Zhang <zhangshuphy@gmail.com>
2023-06-14 21:16:45 +0800

    Update TwoStreamWarmDispersion.m Added some descriptions.

--- TwoStreamWarmDispersion.m
+++ TwoStreamWarmDispersion.m
@@ -1,21 +1,24 @@
+##This scripts solved the equation (1) in [Shu Zhang et al., ion and electron bursts ... Nature Physics 2023]
+##to show the calculation of growth rate's dispersion relation of IAI. 
+
 Z=@(xi) (1i*sqrt(pi)*exp(-xi.^2).*(1+erf(sym(1i*xi))));
 dZdx=@(xi) -2*(1+Z(xi).*xi);
 %dZdx=@(xi) zprimeStevenc(xi);
 wavenumber=0.1:0.01:0.8; %in the unit of 1/lambda_D, definition of LambdaD=sqrt(Te/me)/wpe
-v0=5;%in the unit of sqrt(2Te/me) %0.12 for paper
+v0=0.12;%in the unit of sqrt(2Te/me) %0.12 for paper
 Te=0.2;%keV %$0.2 for paper
-%Ti1=0.4; %keV %0.4 for paper
-Ti1=360;
+Ti1=0.4; %keV %0.4 for paper
+%Ti1=360;
 Ti2=0.7;
 vi1=0;
-vi2=2;
+vi2=2;#for a secondary ion component
 %R=1/100;
-R=1/1836;
-%R=1/1836/65; %for paper
+%R=1/1836;
+R=1/1836/65; %for paper
 %R=1/25;
-%Z=18;%for paper
-Z=1
-n1=1;
+Z=18;%for paper
+%Z=1
+n1=1; #density for first component of the ions
 %Z=18;
 wbunemanreal=zeros(size(wavenumber)); %in the unit of wpe?
 wbunemanimag=wbunemanreal;
